e1d83bd12da757f27eb67f4b54e9e64fa35e74cc
EliseAur <94222653+EliseAur@users.noreply.github.com>
2024-11-12 18:15:53 +0100

    installed tailwind and some organizing

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles/index.css";
 // import App from "./pages/App.jsx";
-import App from "./pages";
+import { App } from "./pages";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
