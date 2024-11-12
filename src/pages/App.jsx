cf8969a2ed3400017d7b9267ea00861f93d80e86
EliseAur <94222653+EliseAur@users.noreply.github.com>
2024-11-12 19:43:21 +0100

    Adding some starter code from Noroff video.

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,30 +1,47 @@
-import { useState } from "react";
-import reactLogo from "../../public/assets/react.svg";
-import viteLogo from "../../public/vite.svg";
-import "../styles/App.css";
+// import { useState } from "react";
+// import reactLogo from "../../public/assets/react.svg";
+// import viteLogo from "../../public/vite.svg";
+// import "../styles/App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+    <div id="main-container" className="container mx-auto">
+      <header className="bg-blue-600 text-white shadow-md">
+        <div className="container mx-auto flex justify-between items-center p-4">
+          <div className="text-lg font-bold">
+            <a href="#" className="hover:text-gray-200">
+              Shop:it
+            </a>
+          </div>
+
+          <nav className="space-x-4">
+            <a href="#" className="hover:text-gray-200">
+              Home
+            </a>
+            <a href="#" className="hover:text-gray-200">
+              About
+            </a>
+            <a href="#" className="hover:text-gray-200">
+              Contact
+            </a>
+            <a href="#" className="hover:text-gray-200">
+              Cart
+            </a>
+          </nav>
+          <div>Login</div>
+        </div>
+      </header>
+      <main className="text-center">
+        <h1 className="text-3xl">Welcome to Shop:it</h1>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis necessitatibus incidunt ducimus in ab ratione voluptas, a possimus aperiam cumque, ipsa quae dolorem doloremque odio.
+          Exercitationem, aspernatur accusamus? Illo, consequatur?
         </p>
-      </div>
-      <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
-    </>
+      </main>
+      <footer>&copy;Noroff School - tutorial React + Vite</footer>
+    </div>
   );
 }
 
