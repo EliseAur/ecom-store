44b0d768f8e2cf33a10abfe76ecb4c37650249f9
EliseAur <94222653+EliseAur@users.noreply.github.com>
2024-11-14 22:44:12 +0100

    Adding links in header

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,19 +1,19 @@
+import { Link } from "react-router-dom";
+
 export default function Header() {
   return (
     <header className="bg-blue-600 text-white shadow-md">
       <div className="container mx-auto flex justify-between items-center p-4">
         <div className="text-lg font-bold">
-          <a href="#" className="hover:text-gray-200">
+          <Link to="/" className="hover:text-gray-200">
             Shop:it
-          </a>
+          </Link>
         </div>
         <nav className="space-x-4">
-          <a href="#" className="hover:text-gray-200">
+          <Link to="/" className="hover:text-gray-200">
             Home
-          </a>
-          <a href="#" className="hover:text-gray-200">
-            About
-          </a>
+          </Link>
+          <Link to="/about">About</Link>
           <a href="#" className="hover:text-gray-200">
             Contact
           </a>
