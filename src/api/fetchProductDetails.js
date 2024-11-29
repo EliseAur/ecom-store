cf8e3871b81d1582af4d1d10e7f3e2e27b4a8bb8
EliseAur <94222653+EliseAur@users.noreply.github.com>
2024-11-29 23:40:53 +0100

    Adding a fetchData function to not repeat myself in the other fetch functions

--- src/api/fetchProductDetails.js
+++ src/api/fetchProductDetails.js
@@ -1,27 +1,34 @@
+import { fetchData } from "./fetchData";
+
 export async function fetchProductDetails(id) {
   const url = `https://v2.api.noroff.dev/online-shop/${id}`;
-
-  try {
-    const response = await fetch(url, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    const result = await response.json();
-
-    if (response.ok) {
-      console.log("Fetched product details:", result.data);
-      return result.data;
-    } else {
-      const error = new Error(result.errors ? result.errors[0].message : "An error occurred");
-      error.status = result.status;
-      error.statusCode = result.statusCode;
-      throw error;
-    }
-  } catch (error) {
-    console.error("Error fetching product details:", error);
-    throw error;
-  }
+  return await fetchData(url);
 }
+
+// export async function fetchProductDetails(id) {
+//   const url = `https://v2.api.noroff.dev/online-shop/${id}`;
+
+//   try {
+//     const response = await fetch(url, {
+//       method: "GET",
+//       headers: {
+//         "Content-Type": "application/json",
+//       },
+//     });
+
+//     const result = await response.json();
+
+//     if (response.ok) {
+//       console.log("Fetched product details:", result.data);
+//       return result.data;
+//     } else {
+//       const error = new Error(result.errors ? result.errors[0].message : "An error occurred");
+//       error.status = result.status;
+//       error.statusCode = result.statusCode;
+//       throw error;
+//     }
+//   } catch (error) {
+//     console.error("Error fetching product details:", error);
+//     throw error;
+//   }
+// }
--- src/api/index.js
+++ src/api/index.js
@@ -1,4 +1,5 @@
 import { fetchProducts } from "./fetchProducts";
 import { fetchProductDetails } from "./fetchProductDetails";
+import { fetchData } from "./fetchData";
 
-export { fetchProducts, fetchProductDetails };
+export { fetchProducts, fetchProductDetails, fetchData };
