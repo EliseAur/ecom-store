ebc3a41b9dbe338251975d67b0a84928461bc9f7
EliseAur <94222653+EliseAur@users.noreply.github.com>
2024-12-09 20:56:11 +0100

    Fixing the productcard so you can see if the product is on sale

--- src/components/productCard.jsx
+++ src/components/productCard.jsx
@@ -1,7 +1,9 @@
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import { calculateDiscount } from "../utils";
 
 export default function ProductCard({ product }) {
+  const discountInfo = calculateDiscount(product);
   return (
     <Link
       to={`/product/${product.id}`}
@@ -13,7 +15,24 @@ export default function ProductCard({ product }) {
         className="w-full h-48 object-cover rounded-md"
       />
       <h2 className="mt-2 text-xl font-bold">{product.title}</h2>
-      <p className="mt-1 text-gray-700">${product.discountedPrice}</p>
+      <p className="mt-1 text-black font-semibold">
+        ${product.discountedPrice}
+        {product.discountedPrice < product.price && (
+          <span className="ml-4 font-normal text-gray-600 line-through italic">
+            <span className="line-through">${product.price.toFixed(2)}</span>
+          </span>
+        )}
+      </p>
+      <p className="font-bold">
+        {discountInfo ? (
+          <span className="text-red-600 text-sm">
+            On sale: -{discountInfo.discountPercentage.toFixed(0)}%
+          </span>
+        ) : (
+          <span className="text-gray-600 text-sm">&nbsp;</span>
+        )}
+      </p>
+
       <Link
         to={`/product/${product.id}`}
         className="mt-4 inline-block bg-blue-600 text-white px-4 py-1 rounded-md hover:bg-blue-700"
@@ -33,5 +52,6 @@ ProductCard.propTypes = {
     }).isRequired,
     title: PropTypes.string.isRequired,
     discountedPrice: PropTypes.number.isRequired,
+    price: PropTypes.number.isRequired,
   }).isRequired,
 };
