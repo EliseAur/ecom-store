3b6d19504f5a3e9d160c8cff321c29e4d2e6d7b8
EliseAur <94222653+EliseAur@users.noreply.github.com>
2024-12-12 14:35:31 +0100

    Added About us text

--- src/pages/about.jsx
+++ src/pages/about.jsx
@@ -1,9 +1,31 @@
 function About() {
   return (
-    <div className="container mx-auto">
+    <div className="container mx-auto p-4 flex-grow">
       <main className="text-center">
-        <h1 className="text-3xl">About us</h1>
-        <p>This text is telling about out business. Woop woop!!!</p>
+        <h1 className="text-3xl">About Us</h1>
+        <p className="p-2">
+          Welcome to our store! We are passionate about providing high-quality
+          products to our customers. Our mission is to offer a wide range of
+          items that cater to various needs and preferences, ensuring that
+          everyone can find something they love.
+        </p>
+        <p className="p-2">
+          We offer a diverse selection of products, including electronics, home
+          goods, fashion, beauty, and more. Each item in our store is carefully
+          curated to meet our high standards of excellence. We work closely with
+          our suppliers to ensure that every product we sell is of the highest
+          quality.
+        </p>
+        <p className="p-2">
+          At our store, we believe in the power of great customer service. Our
+          team is dedicated to providing a seamless shopping experience, from
+          browsing our website to receiving your order. We are always here to
+          help with any questions or concerns you may have.
+        </p>
+        <p className="p-2 pb-4">
+          Thank you for choosing our store. We look forward to serving you and
+          helping you find the perfect products to enhance your life.
+        </p>
       </main>
     </div>
   );
