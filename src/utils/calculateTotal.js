6ed0d94e6b76bfe6d4284c9ab90e2510ba87fb34
EliseAur <94222653+EliseAur@users.noreply.github.com>
2024-12-15 18:41:25 +0100

    removed unused code

--- src/utils/calculateTotal.js
+++ src/utils/calculateTotal.js
@@ -1,11 +1,3 @@
-// export const calculateTotal = (products) => {
-//  return products.reduce((sum, product) => sum + product.discountedPrice * product.quantity, 0);
-// };
-
-// export const calculateTotal = (products) => {
-//  return products.reduce((sum, product) => sum + product.discountedPrice * product.quantity, 0);
-// };
-
 export function calculateTotal(products) {
   return products.reduce(function (sum, product) {
     return sum + product.discountedPrice * product.quantity;
