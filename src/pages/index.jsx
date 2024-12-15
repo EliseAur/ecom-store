910834de60e046bef7f4afba07a4d5421f9ef88c
EliseAur <94222653+EliseAur@users.noreply.github.com>
2024-12-15 18:32:18 +0100

    issues with filenames in deployment in netlify. Try changing to see if it works

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,8 @@
-import Home from "./Home";
-import About from "./About";
-import Contact from "./Contact";
-import Cart from "./Cart";
-import ProductDetail from "./ProductDetail";
+import Home from "./home";
+import About from "./about";
+import Contact from "./contact";
+import Cart from "./cart";
+import ProductDetail from "./productDetail";
 import CheckoutSuccess from "./CheckoutSuccess";
 import ContactSuccess from "./ContactSuccess";
 
