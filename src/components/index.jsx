01805716b661e63ae3064612431742ed798edf79
EliseAur <94222653+EliseAur@users.noreply.github.com>
2024-12-15 18:44:13 +0100

    Same as the last commits

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,5 +1,5 @@
 import Header from "./header";
-import Footer from "./Footer";
+import Footer from "./footer";
 import FilterInput from "./filterInput";
 import ProductCard from "./productCard";
 import HomeContent from "./HomeContent";
